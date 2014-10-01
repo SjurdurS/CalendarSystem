23c061d3731a5ffea7f3ee37a81d34edacfd2ba0
ansuddin <ansuddin7@gmail.com>
2014-10-01 14:36:31 +0200

    Event

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Event.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Event.cs
@@ -8,6 +8,25 @@ namespace CalendarApplication
 {
     class Event
     {
+        public string Name { get; private set; }
+        public string color { get; private set; }
+        public DateTime startDateTime { get; private set; }
+        public DateTime endDateTime { get; private set; }
+        public Alarm alarm { get; private set; }
+        public string place { get; private set; }
+        public string description { get; private set; }
+
+        public Event()
+        {
+            
+        }
+
+
+         
+        
+            
+        }
+
 
     }
 }
