d22986e44807c5a5ecda75c85a055a63c5618e8f
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-01 23:41:59 +0200

    Changes made to the OtherUser class

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/OtherUser.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/OtherUser.cs
@@ -12,5 +12,14 @@ namespace CalendarApplication.Model
         /// The Username of the OtherUser.
         /// </summary>
         public string Username { get; set;}
+
+        /// <summary>
+        /// Instantiate a new OtherUser object.
+        /// </summary>
+        /// <param name="username">The username of the OtherUser</param>
+        public OtherUser(string username)
+        {
+            this.Username = username;
+        }
     }
 }
