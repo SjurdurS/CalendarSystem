6a03983eeea37700d346aa4c6cb0b3ad1cb5354e
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-01 13:52:25 +0200

    Rearranged some classes

--- CalendarApplication/CalendarApplication/UnitTestCalendarApplication/CalendarTests.cs
+++ CalendarApplication/CalendarApplication/UnitTestCalendarApplication/CalendarTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestCalendarApplication
 {
     [TestClass]
-    public class UnitTest1
+    public class CalendarTests
     {
         [TestMethod]
         public void TestMethod1()
