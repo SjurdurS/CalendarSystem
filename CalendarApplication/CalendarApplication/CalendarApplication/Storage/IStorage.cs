23ff62a905eb7f1b3c9c10a88fac17f78a195848
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-01 23:48:07 +0200

    Added some methods to the IStorage interface

--- CalendarApplication/CalendarApplication/CalendarApplication/Storage/IStorage.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Storage/IStorage.cs
@@ -6,8 +6,32 @@ using System.Threading.Tasks;
 
 namespace CalendarApplication.Storage
 {
-    interface IStorage
+    interface IStorage<T>
     {
+        /// <summary>
+        /// Delete an Item from the Database.
+        /// </summary>
+        /// <param name="item">The Item to be deleted.</param>
+        void Delete(T item);
         
+        
+        /// <summary>
+        /// Update an existing item on the Database.
+        /// </summary>
+        /// <param name="item">The Item to be updated.</param>
+        void Update(T item);
+        
+        
+        /// <summary>
+        /// Insert a new Item to the Database.
+        /// </summary>
+        /// <param name="item">The Item to be Inserted.</param>
+        void Insert(T item);
+
+
+        /// <summary>
+        /// Save the changes made to the Database.
+        /// </summary>
+        void SaveChanges();
     }
 }
