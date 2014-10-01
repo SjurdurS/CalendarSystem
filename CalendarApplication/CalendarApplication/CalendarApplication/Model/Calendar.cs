0e129aa2b299f4a3e0203c86237f2e21bb225ce4
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-01 14:36:23 +0200

    Added getters and setters to Calendar

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Calendar.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Calendar.cs
@@ -7,17 +7,17 @@ namespace CalendarApplication.Model
         /// <summary>
         /// The name of the Calendar.
         /// </summary>
-        private string Name;
+        public string Name { get; set; }
         
         /// <summary>
         /// The Color of the Calendar. Used to color the calendar in the View.
         /// </summary>
-        private string Color;
+        private string Color { get; set; }
 
         /// <summary>
         /// List of users this Calendar is shared with.
         /// </summary>
-        private List<OtherUser> SharedGroup; 
+        public List<OtherUser> SharedGroup { get; private set; }
 
 
         /// <summary>
@@ -29,6 +29,7 @@ namespace CalendarApplication.Model
             this.Color = "Blue";
             this.SharedGroup = new List<OtherUser>();
         }
+
 
     }
 }
