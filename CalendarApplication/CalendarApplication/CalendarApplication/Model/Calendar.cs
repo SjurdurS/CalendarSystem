7b985dc0e344d7d6eeaf7a85a2d41b1167082155
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-01 14:25:03 +0200

    Changed Namespace declarations

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Calendar.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Calendar.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CalendarApplication.Model;
+﻿using System.Collections.Generic;
 
-namespace CalendarApplication
+namespace CalendarApplication.Model
 {
     class Calendar
     {
--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Message.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Message.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalendarApplication
+﻿namespace CalendarApplication.Model
 {
     class Message : Alarm // Extends Alarm
     {
