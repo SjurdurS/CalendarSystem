047d46938f62343e5571f6bc7d2bd7d2b3f8ebb3
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-01 14:23:30 +0200

    Added more stuff to the Calendar class

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Calendar.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Calendar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CalendarApplication.Model;
 
 namespace CalendarApplication
 {
@@ -12,7 +13,27 @@ namespace CalendarApplication
         /// The name of the Calendar.
         /// </summary>
         private string Name;
+        
+        /// <summary>
+        /// The Color of the Calendar. Used to color the calendar in the View.
+        /// </summary>
+        private string Color;
 
-        private List<> 
+        /// <summary>
+        /// List of users this Calendar is shared with.
+        /// </summary>
+        private List<OtherUser> SharedGroup; 
+
+
+        /// <summary>
+        /// Instantiate a new Calendar.
+        /// </summary>
+        public Calendar()
+        {
+            this.Name = "Calendar 1";
+            this.Color = "Blue";
+            this.SharedGroup = new List<OtherUser>();
+        }
+
     }
 }
