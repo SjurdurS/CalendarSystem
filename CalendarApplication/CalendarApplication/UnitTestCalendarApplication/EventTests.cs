209ae45f552f50d0e0c401c803ef2cc661d20135
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-02 21:45:42 +0200

    a

--- CalendarApplication/CalendarApplication/UnitTestCalendarApplication/EventTests.cs
+++ CalendarApplication/CalendarApplication/UnitTestCalendarApplication/EventTests.cs
@@ -18,23 +18,6 @@ namespace UnitTestCalendarApplication
             //
         }
 
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
 
         #region Additional test attributes
         //
