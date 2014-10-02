5aefe3def435c844ea840816cf8ee1bab666ca2d
Nicolai Thorndahl <ntho@itu.dk>
2014-10-02 11:01:49 +0200

    Updated Class "Alarm" and "Mail"

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Alarm.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Alarm.cs
@@ -4,12 +4,22 @@ namespace CalendarApplication.Model
 {
     internal class Alarm
     {
-        public DateTime alarmDateTime;
+        public DateTime alarmDateTime; //The date and time that the Client should recieve the notification.
 
         public Alarm(DateTime alarmDateTime)
         {
             this.alarmDateTime = alarmDateTime;
         }
-    
+
+        public DateTime SetAlarmDateTime
+        {
+            get { return alarmDateTime; }
+            set { alarmDateTime = value; }
+        }
+
+        public DateTime GetAlarmDateTime
+        {
+            get { return alarmDateTime; }
+        }
 }
 }
--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Mail.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Mail.cs
@@ -1,6 +1,16 @@
-﻿namespace CalendarApplication.Model
+﻿using System;
+
+namespace CalendarApplication.Model
 {
     class Mail : Alarm // Extends Alarm
     {
+        private string message;
+        private string adress;
+
+        public Mail(DateTime alarmDateTime, string message, string adress) : base(alarmDateTime)
+        {
+            this.message = message;
+            this.adress = adress;
+        }
     }
 }
