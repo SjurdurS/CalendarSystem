018ba793ef7ac9098f80cc40492752dd822c6c5b
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-02 14:58:31 +0200

    Some changes

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/Mail.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/Mail.cs
@@ -4,11 +4,31 @@ namespace CalendarApplication.Model
 {
     class Mail : Alarm // Extends Alarm
     {
+        /// <summary>
+        /// The subject of the mail.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        /// The content of the mail.
+        /// </summary>
         public string Text { get; set; }
+
+        /// <summary>
+        /// The email address.
+        /// </summary>
         public string Address { get; set; }
 
-        public Mail(DateTime dateTime, string message, string address) : base(dateTime)
+        /// <summary>
+        /// Instantiate a new Mail.
+        /// </summary>
+        /// <param name="dateTime">The time the mail should be sent.</param>
+        /// <param name="subject">The Subject of the email.</param>
+        /// <param name="message">The message body of the mail.</param>
+        /// <param name="address">The email address of the mail.</param>
+        public Mail(DateTime dateTime, string subject, string message, string address) : base(dateTime)
         {
+            this.Subject = subject;
             this.Text = message;
             this.Address = address;
         }
