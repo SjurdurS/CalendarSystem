5ab21bc7ddb88a655e38bbbfef236b6d5fd73887
Sjúrður í Sandagerði <sjurdursan@gmail.com>
2014-10-09 17:32:07 +0200

    Added some composite classes

---
+++ CalendarApplication/CalendarApplication/CalendarApplication/Composite Pattern/Composite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalendarApplication.Model;
+
+namespace CalendarApplication.Composite_Pattern
+{
+    class Composite
+    {
+        /// <summary>
+        /// List of events.
+        /// </summary>
+        List<IEvent> eventList = new List<IEvent>();
+
+        public void Add(IEvent eventToAdd)
+        {
+            eventList.Add(eventToAdd);
+        }
+
+
+    }
+}
