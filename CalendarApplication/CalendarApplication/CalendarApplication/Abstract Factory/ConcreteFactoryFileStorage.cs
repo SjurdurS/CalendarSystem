dba36677b31afc7a8a85e473df2580faee5ff60d
ansuddin <ansuddin7@gmail.com>
2014-10-09 16:14:34 +0200

    abstractfactory

---
+++ CalendarApplication/CalendarApplication/CalendarApplication/Abstract Factory/ConcreteFactoryFileStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarApplication.Abstract_Factory
+{
+    class ConcreteFactoryFileStorage
+    {
+        AbstractFileStorage createProductA()
+        {
+            return new FileStorage("filestorage");
+        }
+    }
+}
---
+++ CalendarApplication/CalendarApplication/CalendarApplication/Abstract Factory/ConcreteFactoryStub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarApplication.Abstract_Factory
+{
+    class ConcreteFactoryStub
+    {
+        AbstractProductA createProductA()
+        {
+            return new ProductA1("ProductA1");
+        }
+    }
+}
