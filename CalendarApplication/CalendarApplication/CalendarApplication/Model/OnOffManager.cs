e3e2aa0a06b333f7e6911c40494e9f023cf7ef25
Nicolai Thorndahl <ntho@itu.dk>
2014-10-09 15:08:44 +0200

    Further creation of Strategy pattern

--- CalendarApplication/CalendarApplication/CalendarApplication/Model/OnOffManager.cs
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/OnOffManager.cs
@@ -8,5 +8,21 @@ namespace CalendarApplication.Model
 {
     class OnOffManager
     {
+        private NetworkConnection netCon;
+        private ISynchronizationStrategy netIntf;
+
+
+        public void ConnectionStatus()
+        {
+            if (Online)
+            {
+                netIntf = new OnlineSS();
+            }
+            else //Offline
+            {
+                netIntf = new OfflineSS();
+            }
+            
+        }
     }
 }
