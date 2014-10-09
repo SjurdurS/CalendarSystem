7b2b1f27b63f1f03c5b163ae3bca8da73fe3a68e
Nicolai Thorndahl <ntho@itu.dk>
2014-10-09 13:39:23 +0200

    Added Adapter Pattern classes

---
+++ CalendarApplication/CalendarApplication/CalendarApplication/Model/GoogleAdapter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarApplication.Model
+{
+    /// <summary>
+    /// Part of adapter pattern (The adapter for google calendar)
+    /// </summary>
+    class GoogleAdapter :ICalendarEntry
+    {
+        public void CreateEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateCalendar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShareEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
