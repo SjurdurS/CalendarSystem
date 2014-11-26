5a8e0428cbd9fb45cacbd9e0833fbf7f81a274a1
ansuddin <ansuddin7@gmail.com>
2014-11-26 12:09:17 +0100

    main

--- eCal/eCal/eCal/Program.cs
+++ eCal/eCal/eCal/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,27 +20,37 @@ namespace eCal
         /// <param name="args"></param>
         static void Main(string[] args)
         { 
-            //Set up user
+            //Set up a new user
             if (args.Length == 2)
             {
-                IUser user = new User(args[1], args[2]);
-                //Set up calendars
-                CalendarForm calendarForm = new CalendarForm();
-                Calendar calendar = new Calendar(calendarForm);
-                user.Calendars.Add(calendar);
+                var username = args[1];
+                var mail = args[2];
+                IUser user = new User(username, mail);
 
                 //Set up controllers
-                UpdateControl updateControl = new UpdateControl();
-                NotifyUserControl notifyUserControl = new NotifyUserControl();
-                CalendarControl calendarControl = new CalendarControl();
+                var updateControl = new UpdateControl();
+                var calendarControl = new CalendarControl();
+                
+                //Set up initial calendar
+                var calendarForm = calendarControl.CreatCalendarForm();
+                var calendar = calendarControl.CreateCalendar(calendarForm);
+                user.Calendars.Add(calendar);
 
                 //Set up synchronizer
                 updateControl.AutoSync(user.Calendars);
             }
+            else if (args.Length == 0)
+            {
+                // A Login window opens.
+                // The user logs in with his credentials
+                // If we are online use the RdbmsStorage class to get the User object and load his Calendars.
+                // If we are offline we load the User from localstorage using FileStorage class.
+                // If no User is found with the above credentials the user is prompted with a New User window.
+
+            }
             else
             {
-                //Error message.
-                throw new NotImplementedException();
+                throw new ArgumentException("Wrong number of parameters.");
             }
         }
     }
